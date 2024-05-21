5416982a0041fa1d0a5407161ce19abaf9b2c9fe
mufid1012 <mufidghibran1012@gmail.com>
2024-05-21 14:58:47 +0700

    membuat class mahasiswa

--- initialization.cpp
+++ initialization.cpp
@@ -4,3 +4,23 @@ using namespace std;
 
 // ParadigmaOOP1_0232
 // 15 commit
+
+class mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
+
+public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
+    
+    }
+
+    ~mahasiswa() {
+        cout << "Id    = " << id << endl;
+        cout << "Nama  = " << nama << endl;
+        cout << "Nilai = " << nilai << endl;
+    }
+};
+
+
