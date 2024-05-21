5df6a51af6706451d477a85666fc4791f061c525
mufid1012 <mufidghibran1012@gmail.com>
2024-05-21 18:49:35 +0700

    membuat perintah tambah & cetak untuk dokter & pasien

--- Association/Association.cpp
+++ Association/Association.cpp
@@ -29,3 +29,20 @@ public:
 	void tambahPasien(pasien*); 
 	void cetakPasien(); 
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+	daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter() {
+}
+void dokter::tambahPasien(pasien* pPasien) {
+	daftar_pasien.push_back(pPasien); 
+	pPasien->tambahDokter(this); 
+}
+void dokter::cetakPasien() {
+	cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+	for (auto& a : daftar_pasien) {
+		cout << a->nama << "\n"; 
+	}
+	cout << endl; 
+}
